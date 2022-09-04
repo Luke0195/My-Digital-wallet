54ccd623727c5114c844e379b299971bca7190dd
Lucas <lucas201273@gmail.com>
2022-09-03 23:22:05 -0300

    fix: change script to netifly script

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+//@ts-ignore
 import ReactDOM from 'react-dom/client'
 import App from './components/App'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
